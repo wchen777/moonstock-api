49f8d1a822e39e82afb17d69e98c9c570c21f2c0
wchen77 <william_b_chen@brown.edu>
2021-06-04 18:02:01 -0400

    setup endpoint

--- api/handlers/tickerQuery.go
+++ api/handlers/tickerQuery.go
@@ -2,10 +2,28 @@ package handlers
 
 import (
 	// "database/sql"
+	"log"
+	"net/http"
+
 	_ "github.com/go-sql-driver/mysql"
 	// "github.com/wchen777/moonstock-api/data"
 )
 
-func tickerDataMostRecent() {
+type APILogger struct {
+	l *log.Logger
+}
+
+// instantiate instance of this handler
+func NewStockLogger(l *log.Logger) *APILogger {
+	return &APILogger{l}
+}
+
+func tickerDataMostRecent(rw http.ResponseWriter, r *http.Request) {
+
+	// query database
+
+	// convert data to json
+
+	// send json over
 
 }
